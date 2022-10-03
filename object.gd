3e6947e033dc7240068ba77654e3148c2caa53e5
gustavord <50451202+gustavord@users.noreply.github.com>
2022-10-03 18:47:24 -0300

    Teste

---
+++ object.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var dragging = false
+
+signal dragsignal;
+
+func _ready():
+	connect("dragsignal",self,"_set_drag_pc")
+	
+	
+func _process(delta):
+	if dragging:
+		var mousepos = get_viewport().get_mouse_position()
+		self.position = Vector2(mousepos.x, mousepos.y)
+
+		
+
+func _set_drag_pc():
+	dragging=!dragging
+
+
+func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			print("click")
+			emit_signal("dragsignal")
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			emit_signal("dragsignal")
