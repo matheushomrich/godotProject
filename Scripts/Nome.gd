0c6818c9a3371834d22f7f6cb5ac72754698ff40
gustavord <50451202+gustavord@users.noreply.github.com>
2022-10-28 19:03:16 -0300

    Gerando DOBs e Nomes

---
+++ Scripts/Nome.gd
@@ -0,0 +1,20 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var names = ["Geyer, Gustavo", "Ryu, Hojin", "Horlmreich, Matheus", "Molon, Lucca"]
+func set_random_name():
+	var i: int = randi() % names.size()
+	if names[i]:
+		text = names[i]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_random_name()
+	#text = "Marcelo Cohen"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
